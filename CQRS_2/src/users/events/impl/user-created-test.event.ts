492e092fae41115a57701e9515359a847159ab37
Gromovoi Stanislav <gromila12@rambler.ru>
2023-03-31 15:06:48 +0600

    CQRS_2

---
+++ CQRS_2/src/users/events/impl/user-created-test.event.ts
@@ -0,0 +1,13 @@
+export class UserCreatedEvent {
+    constructor(
+        public readonly userId: string,
+    ) {
+    }
+}
+
+export class UserCreatedEventTest {
+    constructor(
+        public readonly userId: string,
+    ) {
+    }
+}
