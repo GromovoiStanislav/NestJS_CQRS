ebfbe02b6b4f9b732f5dd888d44f74fcf727e634
Gromovoi Stanislav <gromila12@rambler.ru>
2023-04-18 14:25:15 +0600

    CQRS_3

---
+++ CQRS_3/src/campers/camper-entity.repository.ts
@@ -0,0 +1,55 @@
+import { Model } from "mongoose";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Camper } from "./camper.schema";
+import { CamperDto } from "./dto/camper.dto";
+import { ObjectId } from "mongodb";
+
+@Injectable()
+export class CamperDtoRepository {
+  constructor(@InjectModel(Camper.name) private camperModel: Model<Camper>) {
+  }
+
+  async findOneById(id: string): Promise<CamperDto> {
+
+    const camper = await this.camperModel.findOne({ _id: new ObjectId(id) }, {}, { lean: true });
+
+    if (!camper) throw new NotFoundException("Entity was not found.");
+
+    const _id = camper._id.toString();
+    const allergiesLower = camper.allergies.map(allergy => allergy.toLocaleLowerCase());
+    const isAllergicToPeanuts = allergiesLower.includes("peanuts");
+
+    return {
+      ...camper,
+      _id,
+      isAllergicToPeanuts
+    };
+
+  }
+
+  async findAll(): Promise<CamperDto[]> {
+
+    const campers = await this.camperModel.find({}, {}, { lean: true });
+
+    return campers.map(camper => {
+
+      const _id = camper._id.toString();
+      const allergiesLower = camper.allergies.map(allergy => allergy.toLocaleLowerCase());
+      const isAllergicToPeanuts = allergiesLower.includes("peanuts");
+
+      return {
+        ...camper,
+        _id,
+        isAllergicToPeanuts
+      };
+
+    });
+
+  }
+
+
+
+
+
+}
